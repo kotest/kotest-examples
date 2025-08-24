6b82bba561f4c36941b67d140c2e8498472c51ff
sksamuel <sam@sksamuel.com>
2025-08-24 18:01:27 -0500

    Upgrade Kotlin to 2.2.10 and KSP to 2.2.10-2.0.2; add Sonatype Maven snapshots repository; enable additional native targets in `build.gradle.kts`.

--- kotest-native/build.gradle.kts
+++ kotest-native/build.gradle.kts
@@ -3,12 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 plugins {
    alias(libs.plugins.kotlin.multiplatform)
    alias(libs.plugins.kotest)
-   id("com.google.devtools.ksp") version "2.2.0-2.0.2"
-}
-
-repositories {
-   mavenCentral()
-   mavenLocal()
+   alias(libs.plugins.ksp)
 }
 
 kotlin {
@@ -18,9 +13,9 @@ kotlin {
    }
 
    linuxX64()
-//   mingwX64()
-//   macosX64()
-//   iosX64()
+   mingwX64()
+   macosX64()
+   iosX64()
 
    sourceSets {
       nativeTest {
--- kotest-native/settings.gradle.kts
+++ kotest-native/settings.gradle.kts
@@ -2,5 +2,14 @@ pluginManagement {
    repositories {
       gradlePluginPortal()
       mavenLocal()
+      maven("https://central.sonatype.com/repository/maven-snapshots")
+   }
+}
+
+dependencyResolutionManagement {
+   repositories {
+      mavenLocal()
+      mavenCentral()
+      maven("https://central.sonatype.com/repository/maven-snapshots")
    }
 }
