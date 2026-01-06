4df711efd1b3dc6379af78578b6402add7b1b8aa
sksamuel <sam@sksamuel.com>
2026-01-06 14:47:21 -0600

    Refactored `CoroutinesTest` and `DataTests` to simplify nested contexts and reduce delays.

--- kotest-native/src/nativeTest/kotlin/io/kotest/examples/native/CoroutinesTest.kt
+++ kotest-native/src/nativeTest/kotlin/io/kotest/examples/native/CoroutinesTest.kt
@@ -6,12 +6,9 @@ import kotlinx.coroutines.delay
 class CoroutinesTest : FunSpec({
 
    context("kotest native allows nested tests") {
-      delay(100) // look ma, I can use coroutines here!
-      context("give me another context!") {
-         delay(200) // look ma, I can use coroutines here!
-         test("a leaf test") {
-            delay(300) // look ma, I can use coroutines here!
-         }
+      delay(10) // look ma, I can use coroutines here!
+      test("a leaf test") {
+         delay(10) // look ma, I can use coroutines here!
       }
    }
 })
