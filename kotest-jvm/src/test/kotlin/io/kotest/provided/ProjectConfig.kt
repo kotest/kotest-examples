88f3fdcd983f2221394005a9b8f8155c8546448f
sksamuel <sam@sksamuel.com>
2026-01-19 19:34:55 -0600

    Upgrade Gradle wrapper to 9.3.0 and update Kotest to 6.1.0

--- kotest-jvm/src/test/kotlin/io/kotest/provided/ProjectConfig.kt
+++ kotest-jvm/src/test/kotlin/io/kotest/provided/ProjectConfig.kt
@@ -5,11 +5,17 @@ import io.kotest.core.listeners.AfterTestListener
 import io.kotest.core.test.TestCase
 import io.kotest.engine.test.TestResult
 
+/**
+ * An example Kotest extension that prints the duration of each test.
+ */
 object TimerListener : AfterTestListener {
-    override suspend fun afterAny(testCase: TestCase, result: TestResult) =
-        println("- ${testCase.name.name} ${result.duration}")
+   override suspend fun afterAny(testCase: TestCase, result: TestResult) =
+      println("- ${testCase.name.name} ${result.duration}")
 }
 
+/**
+ * Example of product config, registering a Kotest extension.
+ */
 object ProjectConfig : AbstractProjectConfig() {
-    override val extensions = listOf(TimerListener)
+   override val extensions = listOf(TimerListener)
 }
