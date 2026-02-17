39c6eb1efbe1d8c74b2f4a22184ba902ba28386e
sksamuel <sam@sksamuel.com>
2026-02-16 23:33:38 -0600

    Refactor JavaTest to replace `fail` assertion with `assertEquals`

--- kotest-maven/src/test/java/com/JavaTest.java
+++ kotest-maven/src/test/java/com/JavaTest.java
@@ -1,14 +1,14 @@
 package com;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JavaTest {
 
-    @Test
-    void javaTestFails() {
-        fail( "com should fail" );
-    }
+  @Test
+  void javaTestshouldFail() {
+    assertEquals(2, 1 + 1);
+  }
 
 }
