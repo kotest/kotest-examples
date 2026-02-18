31d67fc7ecf4b990dd7b692636e3c6f9c93dc362
sksamuel <sam@sksamuel.com>
2026-02-17 22:06:37 -0600

    Refactor `devServer` configuration to simplify `static` setup in `build.gradle.kts`

--- kotest-multiplatform/build.gradle.kts
+++ kotest-multiplatform/build.gradle.kts
@@ -30,11 +30,9 @@ kotlin {
          val projectDirPath = project.projectDir.path
          commonWebpackConfig {
             devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-               static = (static ?: mutableListOf()).apply {
-                  // Serve sources to debug inside browser
-                  add(rootDirPath)
-                  add(projectDirPath)
-               }
+                  // Serve sources to debug inside the browser
+               static(rootDirPath)
+               static(projectDirPath)
             }
          }
       }
