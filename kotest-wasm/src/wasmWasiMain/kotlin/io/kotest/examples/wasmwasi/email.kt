50b632923bffb734aa7ff80d7364081a2c671078
sksamuel <sam@sksamuel.com>
2026-02-17 22:28:04 -0600

    Remove email and SSN validation examples along with their tests in wasm modules

--- kotest-wasm/src/wasmWasiMain/kotlin/io/kotest/examples/wasmwasi/email.kt
+++
@@ -1,8 +0,0 @@
-package io.kotest.examples.wasmwasi
-
-/**
- * Extremely basic email validator, not intended to do anything complicated, is just for demo purposes.
- */
-fun validateEmail(email: String): Boolean {
-   return email.matches(".+@.+\\..+".toRegex())
-}
