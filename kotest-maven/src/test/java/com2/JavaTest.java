35e33ee1bc8b4aaf25717bfe4e876e9f3f35d63e
sksamuel <sam@sksamuel.com>
2026-03-25 21:35:57 -0500

    Add `JavaTest` with a basic JUnit test case using `assertEquals`

---
+++ kotest-maven/src/test/java/com2/JavaTest.java
@@ -0,0 +1,14 @@
+package com2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JavaTest {
+
+  @Test
+  void javaTestshouldFail() {
+    assertEquals(2, 1 + 1);
+  }
+
+}
